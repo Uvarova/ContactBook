9c1c3fcb8fd54f1f718f1bc4d7181dd9c47d5bf2
ellen.uvarova <vova.petrunko2012@gmail.com>
2019-06-12 15:21:18 +0300

    front && update entities

--- src/main/java/com/contacts/controller/ContactController.java
+++ src/main/java/com/contacts/controller/ContactController.java
@@ -9,33 +9,54 @@ import com.contacts.entity.Contact;
 import com.contacts.repository.ContactRepos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.Map;
-
 @Controller
-
 public class ContactController {
 
     @Autowired
     private ContactRepos contactRepos;
 
-   @GetMapping("contacts")
-    public String contacts(Map<String,Object> model) {
-       return "contacts";
+    @GetMapping("contacts")
+    public String contacts(Model model) {
+        model.addAttribute("result", "");
+        return "contacts";
     }
 
     @PostMapping("contacts")
     public String createContact(@RequestParam(name = "firstName", required = false, defaultValue = "") String firstName,
                                 @RequestParam(name = "lastName", required = false, defaultValue = "") String lastName,
                                 @RequestParam(name = "email", required = false, defaultValue = "") String email,
-                                Map<String, Object> model) {
-       Contact contact = new Contact(firstName,lastName,email);
-       contactRepos.save(contact);
+                                Model model) {
+        Contact contact = new Contact(firstName, lastName, email);
+        contactRepos.save(contact);
+        model.addAttribute("result", "ok");
 
-       return "contacts";
+        return "contacts";
+    }
+
+    @PatchMapping("contacts/patch")
+    public String patchContact(@RequestParam(name = "id", required = false, defaultValue = "") Integer id,
+                               @RequestParam(name = "firstName", required = false, defaultValue = "") String firstName,
+                               @RequestParam(name = "lastName", required = false, defaultValue = "") String lastName,
+                               @RequestParam(name = "email", required = false, defaultValue = "") String email,
+                               Model model) {
+
+        Contact currentContact = contactRepos.findById(id);
+        if (currentContact != null) {
+            currentContact.setFirstName(firstName);
+            currentContact.setLastName(lastName);
+            currentContact.setEmail(email);
+            contactRepos.saveAndFlush(currentContact);
+            model.addAttribute("result", "ok");
+        } else {
+            model.addAttribute("result", "no such contact");
+        }
+
+        return "contacts";
     }
 }
--- src/main/java/com/contacts/controller/ServiceController.java
+++ src/main/java/com/contacts/controller/ServiceController.java
@@ -5,12 +5,12 @@ package com.contacts.controller;
  * class created for project ContactBook
  */
 
-import com.contacts.entity.Contact;
-import com.contacts.repository.ContactRepos;
+import com.contacts.entity.PhoneNumber;
 import com.contacts.repository.PhoneRepos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 import java.util.Map;
@@ -19,25 +19,18 @@ import java.util.Map;
 public class ServiceController {
 
     @Autowired
-    private ContactRepos contactRepos;
-    @Autowired
     private PhoneRepos phoneRepos;
 
-
-    @GetMapping("/")
-    public String greeting(
-            Map<String, Object> model) {
+    @RequestMapping("/")
+    public String greeting() {
         return "main";
     }
 
-    @GetMapping("allcontacts")
-    public String searchMess(Map<String, Object> model) {
-        List<Contact> contacts = contactRepos.findAll();
-      /*  for(Contact next:contacts) {
-            List<PhoneNumber> numbers = phoneRepos.findById(next.getId());
-            model.put("numbers", numbers);
-        }*/
-        //   model.put("contacts", contacts);
+    @GetMapping("/allcontacts")
+    public String showAll(Map<String, Object> map) {
+        List<PhoneNumber> phones = phoneRepos.findAll();
+        map.put("phones",phones);
+
         return "allcontacts";
     }
 
